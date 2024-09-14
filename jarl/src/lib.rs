7540f8262dc081774eabdbce0afa47ff3d3d73dd
A K <alex.kesling@gmail.com>
2024-09-14 13:18:31 -0400

    Add indentation for intended docs nesting from paper comments

--- jarl/src/lib.rs
+++ jarl/src/lib.rs
@@ -45,9 +45,9 @@ pub mod node {
         //   applied to state machine (§5.3)
         // * If last log index ≥ nextIndex for a follower: send AppendEntries RPC with log entries
         //   starting at nextIndex
-        // * If successful: update nextIndex and matchIndex for follower (§5.3)
-        // * If AppendEntries fails because of log inconsistency: decrement nextIndex and retry
-        //   (§5.3)
+        //   * If successful: update nextIndex and matchIndex for follower (§5.3)
+        //   * If AppendEntries fails because of log inconsistency: decrement nextIndex and retry
+        //     (§5.3)
         // * If there exists an N such that N > commitIndex, a majority of matchIndex[i] ≥ N, and
         //   log[N].term == currentTerm: set commitIndex = N (§5.3, §5.4).
 
@@ -68,10 +68,10 @@ pub mod node {
         Candidate<CELL_SIZE, MAX_LOG, VALUE, SNAPSHOT>
     {
         // * On conversion to candidate, start election:
-        // * Increment currentTerm
-        // * Vote for self
-        // * Reset election timer
-        // * Send RequestVote RPCs to all other servers
+        //   * Increment currentTerm
+        //   * Vote for self
+        //   * Reset election timer
+        //   * Send RequestVote RPCs to all other servers
         // * If votes received from majority of servers: become leader
         // * If AppendEntries RPC received from new leader: convert to follower
         // * If election timeout elapses: start new election
